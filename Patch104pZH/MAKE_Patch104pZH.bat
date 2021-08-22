1f425f16290e695f757ce7be070f0e3c848d9c2b
xezon <4720891+xezon@users.noreply.github.com>
2021-08-22 09:56:01 +0200

    Add build scripts for Patch104pZH

---
+++ Patch104pZH/MAKE_Patch104pZH.bat
@@ -0,0 +1,20 @@
+call SETUP_Folders.bat
+
+:: Copy base release files
+xcopy /Y /S %ReleaseUnpackedDir%\* %GeneratedReleaseUnpackedDir%\*
+
+:: Define big file name(s)
+set BigName=600_Patch104pZH
+
+:: Free folders of big file contents
+del /s /f /q %GeneratedBigFilesUnpackedDir%\%BigName%
+del /s /f /q %GeneratedBigFilesDir%\%BigName%.big
+
+:: Copy .big contents
+xcopy /Y /S %GameFilesDir%\*.ini %GeneratedBigFilesUnpackedDir%\%BigName%\
+
+:: Generate .big file(s)
+%ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BigName% -dest %GeneratedBigFilesDir%\%BigName%.big
+
+:: Generate Release file(s)
+xcopy /Y %GeneratedBigFilesDir%\%BigName%.big %GeneratedReleaseUnpackedDir%\%BigName%.big*
