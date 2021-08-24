7f632a57bb3f268cf54dff3a1e465811149784d7
alanblack <31726131+alanblack166@users.noreply.github.com>
2021-08-24 12:45:46 +0300

    Revert "Updated SuperWeaponGeneral.ini to include building scaffold fix"
    This reverts commit 863d9716b548508e5f61321f91f5370d0d1d1464.

--- Patch104pZH/MAKE_Install.bat
+++ Patch104pZH/MAKE_Install.bat
@@ -1,6 +1,6 @@
 call MAKE_Patch104pZH.bat
 
-set GameRootDir="F:\Program Files (x86)\EA Games\clean copy of ZH - Copy"
+set GameRootDir="C:\Program Files (x86)\EA Games\Command & Conquer The First Decade\Command & Conquer(tm) Generals Zero Hour"
 
 ::Copy release files to game
 xcopy /Y /S %GeneratedReleaseUnpackedDir% %GameRootDir%
