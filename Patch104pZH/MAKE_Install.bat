ba112825a1e1c72fa63428a9a7b816fb8a043cf9
xezon <4720891+xezon@users.noreply.github.com>
2021-09-04 08:01:57 +0200

    Build: Install scripts will request admin privileges and patch unrelated files can be automatically voided for test session (#175)
    * Install scripts will now request admin privileges to make install easier for users who have game installed on C drive
    * Add user configurable list of files that are automatically voided (renamed) before starting game for testing

--- Patch104pZH/MAKE_Install.bat
+++ Patch104pZH/MAKE_Install.bat
@@ -1,3 +1,30 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+::  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+echo on
+
 call MAKE_Patch104pZH.bat
 call SETUP_UserSettings.bat
 
