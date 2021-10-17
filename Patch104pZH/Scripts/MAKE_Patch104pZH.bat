7eb78c5901d0f59b31f541b557879a8a4b0986c9
commy2 <commy-2@gmx.de>
2021-10-17 13:57:09 +0200

    Add: Mammoth Tank Default Implementation With Textures From CCG Alpha Version

--- Patch104pZH/Scripts/MAKE_Patch104pZH.bat
+++ Patch104pZH/Scripts/MAKE_Patch104pZH.bat
@@ -16,15 +16,17 @@ if "%ThisArg1%"=="build" (
     :: All files listed here become part of the core of Patch104p and are meant
     :: to be critical for client compatibility and essential for functionality.
     :: Optional files should be moved into one of the other scripts.
-    xcopy /y /s "%GameFilesDir%\*.ini"                     "%GeneratedBigFilesUnpackedDir%\%BigName%\"
-    xcopy /y /s "%GameFilesDir%\*.wnd"                     "%GeneratedBigFilesUnpackedDir%\%BigName%\"
-    xcopy /y    "%GameFilesDir%\Art\Textures\exlaser3.dds" "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\Textures\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\ABSWGLink_L.W3D"   "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\AVAvnger.W3D"      "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\AVAvnger_D.W3D"    "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\EXCarptBmb2.W3D"   "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\NVLOutpost.W3D"    "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
-    xcopy /y    "%GameFilesDir%\Art\W3D\NVLOutpost_D.W3D"  "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y /s "%GameFilesDir%\*.ini"                            "%GeneratedBigFilesUnpackedDir%\%BigName%\"
+    xcopy /y /s "%GameFilesDir%\*.wnd"                            "%GeneratedBigFilesUnpackedDir%\%BigName%\"
+    xcopy /y    "%GameFilesDir%\Art\Textures\exlaser3.dds"        "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\Textures\"
+    xcopy /y    "%GameFilesDir%\Art\Textures\gxmammoth_co.tga"    "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\Textures\"
+    xcopy /y    "%GameFilesDir%\Art\Textures\gxmammothalt_HI.tga" "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\Textures\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\ABSWGLink_L.W3D"          "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\AVAvnger.W3D"             "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\AVAvnger_D.W3D"           "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\EXCarptBmb2.W3D"          "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\NVLOutpost.W3D"           "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
+    xcopy /y    "%GameFilesDir%\Art\W3D\NVLOutpost_D.W3D"         "%GeneratedBigFilesUnpackedDir%\%BigName%\Art\W3D\"
     
     :: Generate .big file(s)
     "%ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe" -source "%GeneratedBigFilesUnpackedDir%\%BigName%" -dest "%GeneratedBigFilesDir%\%BigName%.big"
