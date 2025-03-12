aa46c778240496090f07eeb3750501c72053e3ba
Polypheides <rivaljohansson@gmail.com>
2025-03-12 12:33:28 +0100

    Update fix_string_errors.py

--- Patch104pZH/Design/Scripts/str/fix_string_errors.py
+++ Patch104pZH/Design/Scripts/str/fix_string_errors.py
@@ -43,7 +43,8 @@ def run():
             startswith_nocase(line, "BP: \"") or
             startswith_nocase(line, "PL: \"") or
             startswith_nocase(line, "RU: \"") or
-            startswith_nocase(line, "UK: \"")):
+            startswith_nocase(line, "UK: \"") or
+            startswith_nocase(line, "SE: \"")):
             if line[5:6] != "*":
                 new_line = line[:5] + "*" + line[5:]
                 generals_str_lines[index] = new_line
